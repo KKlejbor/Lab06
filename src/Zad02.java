b1a238d8aed70d77ecdaf8e6bbf9895267b1eea7
Krzysztof Klejbor <krzysztof.klejbor@gmail.com>
2020-04-23 16:04:31 +0200

    Dodaj nowy kod wykonujący operacje na liczbach

--- src/Zad02.java
+++ src/Zad02.java
@@ -1,8 +1,12 @@
+import javax.print.attribute.standard.MediaSize;
+
 public class Zad02 {
     public static void main(String[] args) {
         String wyrażenieONP = "47 6 9 * +";
         Stos<String> stosONP = new Stos<>();
         String liczba = "";
+        int liczba1, liczba2;
+        boolean NAN = false;
 
         for (int i = 0; i < wyrażenieONP.length(); i++) {
             char znak = wyrażenieONP.charAt(i);
@@ -19,26 +23,65 @@ public class Zad02 {
                 case '9':
                     liczba += znak;
                     break;
+
                 case ' ':
                     stosONP.dodajNaStos(liczba);
                     liczba = "";
                     break;
+
                 case '+':
-                    stosONP.dodajNaStos("+");
+//                    stosONP.dodajNaStos("+");
+                    liczba2 = Integer.parseInt(stosONP.usuńZeStosu());
+                    liczba1 = Integer.parseInt(stosONP.usuńZeStosu());
+                    stosONP.dodajNaStos(Integer.toString(liczba1+liczba2));
                     break;
+
                 case '-':
-                    stosONP.dodajNaStos("-");
+//                    stosONP.dodajNaStos("-");
+                    liczba2 = Integer.parseInt(stosONP.usuńZeStosu());
+                    liczba1 = Integer.parseInt(stosONP.usuńZeStosu());
+                    stosONP.dodajNaStos(Integer.toString(liczba1-liczba2));
                     break;
+
                 case '*':
-                    stosONP.dodajNaStos("*");
+//                    stosONP.dodajNaStos("*");
+                    liczba2 = Integer.parseInt(stosONP.usuńZeStosu());
+                    liczba1 = Integer.parseInt(stosONP.usuńZeStosu());
+                    stosONP.dodajNaStos(Integer.toString(liczba1*liczba2));
                     break;
+
                 case '/':
-                    stosONP.dodajNaStos("/");
+//                    stosONP.dodajNaStos("/");
+                    liczba2 = Integer.parseInt(stosONP.usuńZeStosu());
+                    liczba1 = Integer.parseInt(stosONP.usuńZeStosu());
+
+                    if(liczba2 != 0) {
+                        stosONP.dodajNaStos(Integer.toString(liczba1 / liczba2));
+                    }
+
+                    else {
+                        NAN = true;
+                    }
+
                     break;
+
                 default:
                     break;
             }
+
+            if(NAN){
+                System.out.println("BŁĄD! Nie można dzielić przez zero!");
+                System.out.println(wyrażenieONP);
+                for (int j = 0; j < i; j++) {
+                    System.out.print(' ');
+                }
+                System.out.println("^");
+                System.out.println("Stos: ");
+                stosONP.wydrukujStos();
+                break;
+            }
             stosONP.wydrukujStos();
+            System.out.println(i+"\n-----------------------");
         }
     }
 }
